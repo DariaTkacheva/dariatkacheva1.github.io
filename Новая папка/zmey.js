6bf7f6f2546f8a13e2694aae5ac1492160da1e31
DariaTkacheva <90569700+DariaTkacheva@users.noreply.github.com>
2021-11-20 15:12:09 +0300

    Update zmey.js

--- Новая папка/zmey.js
+++ Новая папка/zmey.js
@@ -32,13 +32,20 @@ let dir;
 
 function direction(event){
     if(event.keyCode==37 && dir !="right")
-    dir="left";
+   { dir="left";
+   fly.play();
+}
     else if(event.keyCode==38&& dir !="down")
-    dir="up";
+    {dir="up";
+    fly.play();
+}
     else if(event.keyCode==39&& dir !="left")
-    dir="right";
+    {dir="right";
+    fly.play();
+}
     else if(event.keyCode==40&& dir !="up")
-    dir="down";
+   { dir="down";
+   fly.play();}
 }
 
 function eatTail(head, arr){
@@ -73,8 +80,6 @@ function drawGame()
     snake.pop();
 }
     
-if(score==1)
-     fly.play();
     
 if(snakeX<box || snakeX>box*17 || snakeY<3*box|| snakeY>box*17)
     clearInterval(game);
